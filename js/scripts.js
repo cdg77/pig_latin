215274c931bbd300a8ca6e204a1bd3ec58a87315
Carlo De Gregorio & Toma Apostolov <student@epicodus.com>
2015-09-09 15:41:39 -0700

    pass specs for checking if the letter is a consonant. created a separate function for that.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,28 @@
-var pigLatinize = function(string){
+var pigLatinize = function(string) {
 
   if (string.startsWith("a"||"e"||"o"||"u"||"i", 0)) {
     var pigString = string.concat("ay");
     return pigString;
   } else {
-
     var stringArray = string.split('');
-    var firstLetterShifted = stringArray.shift();
 
-     stringArray.push(firstLetterShifted);
+    do {
+      var firstLetterShifted = stringArray.shift();
+      stringArray.push(firstLetterShifted);
+      var firstLetter = stringArray[0];
+
+    } while (isConsonant(firstLetter) );
+
     var pigString = stringArray.join('').concat('ay');
     return pigString;
+  }
+}
+
+var isConsonant = function(letter) {
+  var vowels = ["a","e","i","o","u"];
+  if (vowels.indexOf(letter) === -1) {
+    return true;
+  } else {
+    return false;
   }
 }
--- spec/specs.js
+++ spec/specs.js
@@ -4,10 +4,16 @@ describe('pigLatinize', function() {
   });
 
   it("check if the begins with one consonants moves the first consonant to the end adds AY after.", function(){
-    expect(pigLatinize("photography")).to.equal("hotographypay");
+    expect(pigLatinize("potography")).to.equal("otographypay");
   });
 
   it("check if the begins with one or more consonants, move all of the first consecutive ones to the end, and add AY after.", function(){
     expect(pigLatinize("photography")).to.equal("otographyphay");
+  });
+});
+
+describe('isConsonant', function() {
+  it("check if the letter is a consonant", function(){
+    expect(isConsonant("photography")).to.equal(true)
   });
 });
